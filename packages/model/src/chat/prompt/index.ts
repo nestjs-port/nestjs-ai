b8a81bd931a9ec832b4fb7ca2cbbcd53f8c1b87e
Jake Son <sonjeabin@gmail.com>
2026-01-20 19:58:16 +0900

    Add copy() method to ChatOptions interface

--- packages/model/src/chat/prompt/index.ts
+++ packages/model/src/chat/prompt/index.ts
@@ -1,2 +1,1 @@
-export type { ChatOptions, ChatOptionsBuilder } from "./chat-options.interface";
-export { chatOptionsBuilder } from "./chat-options.interface";
+export type { ChatOptions } from "./chat-options.interface";

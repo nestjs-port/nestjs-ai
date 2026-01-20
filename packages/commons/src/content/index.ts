18db0c1279b9bce8db3ab81850aa724480a430fe
Jake Son <sonjeabin@gmail.com>
2026-01-20 21:31:42 +0900

    refactor(commons): refactor Media class to use RORO pattern
    - Replace multiple constructor overloads with single constructor using options object
    - Rename MediaOptions to CreateMediaOptions for clarity
    - Remove uri parameter, use only data parameter
    - Remove MediaBuilder class and static factory methods (fromUri, fromData)
    - Convert MediaFormat from const object to enum
    - Update exports in index.ts

--- packages/commons/src/content/index.ts
+++ packages/commons/src/content/index.ts
@@ -1,8 +1,3 @@
 export type { Content } from "./content.interface";
-export {
-	Media,
-	MediaBuilder,
-	MediaFormat,
-	type MediaFormatType,
-} from "./media";
+export { CreateMediaOptions, Media, MediaFormat } from "./media";
 export type { MediaContent } from "./media-content.interface";

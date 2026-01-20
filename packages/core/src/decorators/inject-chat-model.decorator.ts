db60f8f33d1ff758bfbd647c0101d864135c30ca
Jake Son <sonjeabin@gmail.com>
2026-01-20 20:05:43 +0900

    docs(core): add JSDoc comment to InjectChatModel decorator

--- packages/core/src/decorators/inject-chat-model.decorator.ts
+++ packages/core/src/decorators/inject-chat-model.decorator.ts
@@ -1,4 +1,8 @@
 import { Inject } from "@nestjs/common";
 import { CHAT_MODEL_TOKEN } from "../constants";
 
-export const InjectChatModel = () => Inject(CHAT_MODEL_TOKEN);
+/**
+ * Decorator that injects the chat model instance.
+ */
+export const InjectChatModel = (): ParameterDecorator =>
+	Inject(CHAT_MODEL_TOKEN);

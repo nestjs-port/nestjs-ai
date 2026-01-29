95962e240f7e619c7f6c9cb669ddc6f67458410d
Jake Son <sonjeabin@gmail.com>
2026-01-29 13:07:03 +0900

    feat(model): add ModelDescription and ChatModelDescription interfaces
    - Add ModelDescription interface for AI model metadata (name, description, version)
    - Add ChatModelDescription as marker type alias for chat model metadata
    - Export new types from model index

---
+++ packages/model/src/model/chat-model-description.interface.ts
@@ -0,0 +1,7 @@
+import type { ModelDescription } from "./model-description.interface";
+
+/**
+ * Marker interface, to be used to store info on the model such as the current context
+ * length.
+ */
+export type ChatModelDescription = ModelDescription;

ccdbcc2d8ade622d23a90beaedc3cd0988343a8a
mooowu <me@mooowu.xyz>
2026-02-01 15:50:52 +0900

    feat(commons): add Content and Part support for generative AI

--- packages/commons/src/content/index.ts
+++ packages/commons/src/content/index.ts
@@ -1,3 +1,6 @@
+export { ContentBuilder } from "./content.builder";
+export { Content as ContentClass } from "./content.class";
 export type { Content } from "./content.interface";
 export { Media, MediaFormat, MediaOptionsProps } from "./media";
 export type { MediaContent } from "./media-content.interface";
+export type { Part } from "./part.interface";

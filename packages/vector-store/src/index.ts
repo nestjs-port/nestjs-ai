e80ec4caf9eec409b25c78af6783e016a8d545de
Jake Son <sonjeabin@gmail.com>
2026-02-22 16:51:05 +0900

    refactor: simplify vector-store exports
    - Remove the empty aot index export from vector-store
    - Drop the aot re-export from the package entry point
    - Export SearchRequest and SearchRequestBuilder from index.ts

--- packages/vector-store/src/index.ts
+++ packages/vector-store/src/index.ts
@@ -1,6 +1,6 @@
-export * from "./aot";
 export * from "./filter";
 export * from "./observation";
 export * from "./properties";
+export { SearchRequest, SearchRequestBuilder } from "./search-request";
 export { SpringAIVectorStoreTypes } from "./spring-ai-vector-store-types";
 export { VectorStoreRetriever } from "./vector-store-retriever";

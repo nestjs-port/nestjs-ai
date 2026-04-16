6f761e874183a9c1df7110d74340fe228604a452
Jake Son <sonjeabin@gmail.com>
2026-04-16 12:12:50 +0900

    feat(model): port image model abstractions from Java
    - Add Image, ImageGeneration, ImageResponse, and metadata types
    - Add ImageModel interface as the ImagePrompt/ImageResponse specialization
    - Update the image barrel exports for the new public APIs
    
    Signed-off-by: Jake Son <sonjeabin@gmail.com>

---
+++ packages/model/src/image/image-response-metadata.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AbstractResponseMetadata } from "../model";
+
+/**
+ * Represents metadata associated with an image response.
+ */
+export class ImageResponseMetadata extends AbstractResponseMetadata {
+  private readonly _created: number | null;
+
+  constructor(created?: number | null) {
+    super();
+    this._created = created ?? Date.now();
+  }
+
+  get created(): number | null {
+    return this._created;
+  }
+}

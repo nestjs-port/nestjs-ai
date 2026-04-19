32336fb977d9c49f948d236eb433dbbbef3c25f7
Jake Son <sonjeabin@gmail.com>
2026-04-20 08:33:09 +0900

    feat(model-anthropic): add chat module coverage
    - Add the Anthropic chat model module and property wrapper for NestJS DI.
    
    - Add unit-style and integration-style tests that mirror the OpenAI SDK module conventions.
    
    - Register the Anthropic package in integration-tests and Vitest alias resolution.
    
    Signed-off-by: Jake Son <sonjeabin@gmail.com>

---
+++ packages/models/anthropic/src/module/index.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2023-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export * from "./anthropic-chat-model.module";
+export type { AnthropicChatProperties } from "./anthropic-chat-properties";
+export { ANTHROPIC_CHAT_DEFAULT_MODEL } from "./anthropic-chat-properties";

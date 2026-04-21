81745716627218d6579fcef70a44c9a4b735d2aa
Jake Son <sonjeabin@gmail.com>
2026-04-22 07:54:49 +0900

    feat(model): add moderation abstractions
    - Add moderation domain types, response wrappers, and helpers.
    - Export the moderation surface from the model package barrel.
    
    Signed-off-by: Jake Son <sonjeabin@gmail.com>

---
+++ packages/model/src/moderation/index.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { Categories, type CategoriesProps } from "./categories";
+export { CategoryScores, type CategoryScoresProps } from "./category-scores";
+export {
+  Generation as ModerationGeneration,
+  type ModerationGenerationProps,
+} from "./generation";
+export { Moderation, type ModerationProps } from "./moderation";
+export type { ModerationGenerationMetadata } from "./moderation-generation-metadata";
+export { ModerationMessage } from "./moderation-message";
+export type { ModerationModel } from "./moderation-model.interface";
+export type { ModerationOptions } from "./moderation-options";
+export {
+  ModerationOptionsBuilder,
+  type ModerationOptionsProps,
+} from "./moderation-options-builder";
+export { ModerationPrompt } from "./moderation-prompt";
+export { ModerationResponse } from "./moderation-response";
+export { ModerationResponseMetadata } from "./moderation-response-metadata";
+export {
+  ModerationResult,
+  type ModerationResultProps,
+} from "./moderation-result";
